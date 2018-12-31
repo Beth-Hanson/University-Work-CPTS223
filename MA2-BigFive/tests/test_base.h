40b69ec7f95217d9b4e7105abe9d8a7b10a9317f
Aaron S. Crandall <acrandal@gmail.com>
2018-12-31 13:54:42 -0800

    Should have a move constructor test

--- MA2-BigFive/tests/test_base.h
+++ MA2-BigFive/tests/test_base.h
@@ -53,6 +53,32 @@ TEST(BaseLinkedListBigFive, CopyConstructorPointerTest)
     // Assert
     ASSERT_NE(firstList.getFront(), listCopy.getFront());
 }
+//*** End of Copy Constructor tests ***
+
+//*** Start of Move Constructor tests ***
+TEST(BaseLinkedListBigFive, MoveConstructor)
+{
+    LinkedList<int> firstList{};
+    vector<int> srcVals = {1, 2, 3, 4, 5};
+    for (auto val : srcVals)
+        { firstList.addElement(val); }
+    // Act
+    ListNode<int>* srcFront = firstList.getFront();     // Save pointer for testing
+    LinkedList<int> movedList = std::move(firstList);   // Move constructor
+
+    vector<int> movedListVals = {};
+    for (int i = 0; i < movedList.getSize(); i++)
+    {
+        movedListVals.push_back(movedList.getElementAt(i));
+    }
+    // Assert
+    ASSERT_EQ(srcFront, movedList.getFront())               // Should be same Nodes
+    ASSERT_THAT(movedListVals, ElementsAreArray(srcVals));  // Elements all in order
+    ASSERT_EQ(srcVals.size(), movedListVals.size());        // No elements lost
+    ASSERT_EQ(srcVals.size(), movedList.getSize());         // List knows size properly
+    ASSERT_EQ(nullptr, firstList.getFront());               // Old list destroyed
+    ASSERT_EQ(0, firstList.getSize());                      // Old list empty
+}
 
 
 //********* End of Base Microassignment LinkedList tests **************//
